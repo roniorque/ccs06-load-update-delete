0b0e52f18c78575d75fa05c794798c656e06609c
Ronio Jerico Roque <roniojericho144@gmail.com>
2023-05-08 16:06:15 +0800

    added data fields name

--- index.php
+++ index.php
@@ -11,6 +11,17 @@ $pets = pets::list();
 <h1>Pets</h1>
 
 <table border="1" cellpadding="5">
+<tr>
+	<th>ID</th>
+	<th>NAME</th>
+	<th>GENDER</th>
+	<th>BIRTHDATE</th>
+	<th>OWNER</th>
+	<th>EMAIL</th>
+	<th>ADDRESS</th>
+	<th>CONTACT</th>
+	<th colspan= "4"> EDIT INFO</th>
+</tr>
 <?php foreach ($pets as $pets): ?>
 <tr>
 <td><?php echo $pets->getId(); ?></td>
@@ -19,7 +30,11 @@ $pets = pets::list();
 <td><?php echo $pets->getBirthdate(); ?></td>
 <td><?php echo $pets->getOwner(); ?></td>
 <td><?php echo $pets->getEmail(); ?></td>
+<td><?php echo $pets->getAddress(); ?></td>
 <td><?php echo $pets->getContactNumber(); ?></td>
+<td>
+	<a href="insert.php?id=<?php echo $pets->getId(); ?>">INSERT</a>
+</td>
 <td>
 	<a href="edit-pets.php?id=<?php echo $pets->getId(); ?>">EDIT</a>
 </td>
@@ -32,3 +47,4 @@ $pets = pets::list();
 </tr>
 <?php endforeach ?>
 </table>
+	
